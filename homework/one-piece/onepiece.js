58b03fcfec3140496bf2bc6832702aaa9da5036d
mienweng <sb93129@gmail.com>
2023-06-11 23:52:40 +0800

    onepiece.html

--- homework/one-piece/onepiece.js
+++ homework/one-piece/onepiece.js
@@ -8,35 +8,44 @@ const characters = [
     {
         name: 'Zoro',
         image: 'images/zoro.jpg',
+        link: 'https://zh.wikipedia.org/zh-tw/%E7%BE%85%E7%BE%85%E8%AB%BE%E4%BA%9E%C2%B7%E7%B4%A2%E9%9A%86'
     },
     {
         name: 'Nami',
         image: 'images/nami.jpg',
+        link: 'https://zh.wikipedia.org/zh-tw/%E5%A8%9C%E7%BE%8E'
     },
     {
         name: 'Usopp',
         image: 'images/usopp.jpg',
+        link: 'https://zh.wikipedia.org/zh-tw/%E9%A8%99%E4%BA%BA%E5%B8%83'
     },
     {
         name: 'Sanji',
         image: 'images/sanji.jpg',
+        link: 'https://zh.wikipedia.org/zh-tw/%E8%B3%93%E4%BB%80%E8%8E%AB%E5%85%8B%C2%B7%E9%A6%99%E5%90%89%E5%A3%AB'
     },
     {
         name: 'Chopper',
         image: 'images/chopper.jpg',
+        link: 'https://zh.wikipedia.org/zh-tw/%E5%A4%9A%E5%B0%BC%E5%A4%9A%E5%B0%BC%C2%B7%E5%96%AC%E5%B7%B4'
     },
     {
         name: 'Robin',
         image: 'images/robin.jpg',
+        link: 'https://zh.wikipedia.org/zh-tw/%E5%A6%AE%E5%8F%AF%C2%B7%E7%BE%85%E8%B3%93'
     }, {
         name: 'Franky',
         image: 'images/franky.jpg',
+        link: 'https://zh.wikipedia.org/zh-tw/%E4%BD%9B%E6%9C%97%E5%9F%BA'
     }, {
         name: 'Brook',
         image: 'images/brook.jpg',
+        link: 'https://zh.wikipedia.org/zh-tw/%E5%B8%83%E9%AD%AF%E5%85%8B_(ONE_PIECE)'
     }, {
         name: 'Jinbe',
         image: 'images/jinbe.jpg',
+        link: 'https://zh.wikipedia.org/zh-tw/%E5%90%89%E8%B2%9D%E7%88%BE'
     },
 
 
